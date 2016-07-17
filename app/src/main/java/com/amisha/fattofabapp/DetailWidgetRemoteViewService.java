5b2fd76acf7c5653dc1017486fcb20be54ee8d28
m1 <amisha.saya@gmail.com>
2016-07-18 02:54:24 +0530

    Widget.

---
+++ app/src/main/java/com/amisha/fattofabapp/DetailWidgetRemoteViewService.java
@@ -0,0 +1,96 @@
+package com.amisha.fattofabapp;
+
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Binder;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+public class DetailWidgetRemoteViewService extends RemoteViewsService
+{
+    private static final String[] EXERCISES_COLUMNS = {
+            ExerciseProvider.DATABASE_NAME + "." + ExerciseProvider.NAME,
+    };
+
+    static final int INDEX_EXERCISES_ID = 0;
+    static final int INDEX_EXERCISE_NAME = 1;
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsFactory() {
+            private Cursor cursor=null;
+            @Override
+            public void onCreate() {
+
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if(cursor!=null)
+                {
+                    cursor.close();
+                }
+                final long identityToken= Binder.clearCallingIdentity();
+                cursor=getContentResolver().query(ExerciseProvider.CONTENT_URI,EXERCISES_COLUMNS,ExerciseProvider.EXERCISES_ID
+                        + "=?",new String[] {"1"},null);
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if(cursor!=null)
+                {
+                    cursor.close();
+                    cursor=null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return cursor==null ? 0 : cursor.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if(position== AdapterView.INVALID_POSITION || cursor==null || !cursor.moveToPosition(position))
+                {
+                    return null;
+                }
+                RemoteViews remoteViews=new RemoteViews(getPackageName(), R.layout.widget_layout);
+                String name=cursor.getString(INDEX_EXERCISE_NAME);
+
+
+                remoteViews.setTextViewText(R.id.widgetText, name);
+
+                final Intent finalIntent=new Intent();
+             //   finalIntent.setData(ExerciseProvider);
+                remoteViews.setOnClickFillInIntent(R.id.widget_list,finalIntent);
+                return remoteViews;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(),R.layout.widget_layout);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if(cursor.moveToPosition(position))
+                    return cursor.getInt(INDEX_EXERCISES_ID);
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+}
--- app/src/main/java/com/amisha/fattofabapp/MyWidgetProvider.java
+++ app/src/main/java/com/amisha/fattofabapp/MyWidgetProvider.java
@@ -1,12 +1,64 @@
 package com.amisha.fattofabapp;
 
 
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
+import android.widget.RemoteViews;
 
 public class MyWidgetProvider extends AppWidgetProvider {
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        for(int i=0;i<appWidgetIds.length;i++) {
+
+            RemoteViews remoteViews=new RemoteViews(context.getPackageName(),R.layout.widget_detail);
+            super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+            Intent intent=new Intent(context,ExercisesActivity.class);
+            PendingIntent pendingIntent=PendingIntent.getActivity(context,0,intent,0);
+            remoteViews.setOnClickPendingIntent(R.id.widget,pendingIntent);
+
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.ICE_CREAM_SANDWICH){
+                setRemoteAdapter(context,remoteViews);
+            } else{
+                setRemoteAdapterr(context,remoteViews);
+            }
+            boolean detailActivity=context.getResources().getBoolean(R.bool.detail_Activity);
+            Intent clickIntent=detailActivity ? new Intent(context,MainActivity.class) : new Intent(context,ExercisesActivity.class);
+            PendingIntent clickPendingIntent= TaskStackBuilder.create(context).addNextIntentWithParentStack(clickIntent)
+                    .getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setPendingIntentTemplate(R.id.widget_list,clickPendingIntent);
+            remoteViews.setEmptyView(R.id.widget_list,R.id.widget_empty);
+            appWidgetManager.updateAppWidget(appWidgetIds[i],remoteViews);
+        }
+    }
+
+    public void onReceive(@NonNull Context context, @NonNull Intent intent)
+    {
+        super.onReceive(context, intent);
+        AppWidgetManager appWidgetManager=AppWidgetManager.getInstance(context);
+        int[] appWidgetIds=appWidgetManager.getAppWidgetIds(new ComponentName(context,getClass()));
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews remoteViews)
+    {
+        remoteViews.setRemoteAdapter(R.id.widget_list, new Intent(context, DetailWidgetRemoteViewService.class));
+    }
+
+    @SuppressWarnings("deprecation")
+    private void setRemoteAdapterr(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0,R.id.widget_list,
+                new Intent(context,DetailWidgetRemoteViewService.class));
     }
 }
